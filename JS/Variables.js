2db7ee323e9bd24d19ad001e72d409a36b43ccf2
PNRojo <rojopablonicolas@gmail.com>
2023-03-27 17:33:47 -0300

    variables

---
+++ JS/Variables.js
@@ -0,0 +1,20 @@
+//variables guardan elementos para consultarlos o modificarlos fácilemente
+//esos elementos son datos y son de diferentes tipos.
+// String (palabras o cadenas de texto)
+var bootcamp = 'Henry';
+
+// Números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Booleanos (palabras reservadas que no van con comillas)
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined (aún no le asignamos valor a la variable)
+var cajon1;
+console.log(cajon1);
+
+// Null (asigna a propósito el valor nulo)
+var sinNada = null;
